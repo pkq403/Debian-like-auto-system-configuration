7010405658cbb23b54c6bcc285e248f57dfa8604
pedrocastro <pcc454@alumnos.unican.es>
2025-04-17 13:12:42 +0200

    new configurations for nvim, zsh, ohmyzsh and power10k

--- nvim-config/nvim-config.sh
+++ nvim-config/nvim-config.sh
@@ -8,9 +8,4 @@ apt install build-essential
 apt install ripgrep
 # Clipboard management tool
 apt install xclip
-# Installing a nerd font (Jetbrains Mono)
-mkdir jetbrains-mono && cd jetbrains-mono
-wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip?_gl=1*1w4mw*_ga*MTU0MzQ3NzM5OC4xNzQ0NDQ4Njg2*_ga_9J976DJZ68*MTc0NDQ0ODY4NC4xLjAuMTc0NDQ0ODY4OC42MC4wLjA.*_gcl_au*NTc1MjQ5MzM0LjE3NDQ0NDg2ODk.*FPAU*NTc1MjQ5MzM0LjE3NDQ0NDg2ODk. -O JetBrainsMono.zip
-unzip JetBrainsMono.zip
-cp fonts/ttf/* /usr/share/fonts/ && cd .. && rm -rf jetbrains-mono
 
--- zsh-config/zsh-config.sh
+++ zsh-config/zsh-config.sh
@@ -1,2 +1,39 @@
 #!/bin/bash
 
+# installing zsh
+sudo apt install zsh
+# Setting zsh as the deafult shell
+chsh -s %(which zsh)
+
+# Installing ohyzsh
+# this will create a custom .zshrc and a .ohmyzsh/ (folder) where will be stored ohmyzsh config
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# powerlevel10k theme installation
+# downloading the recommended font
+
+# - MesloLGSNFRegular.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -O MesloLGSNFRegular.ttf
+
+# - MesloLGSNFBold.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -O MesloLGSNFBold.ttf
+
+# - MesloLGSNFItalic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -O MesloLGSNFItalic.ttf
+
+# - MesloLGSNFBoldItalic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -O MesloLGSNFBoldItalic.ttf
+
+# Installing fonts
+mv MesloLGSNFRegular.ttf MesloLGSNFBold.ttf MesloLGSNFItalic.ttf MesloLGSNFBoldItalic.ttf /usr/share/fonts
+
+# Installing Power10k itself
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+# Setting it in .zshrc
+sed -i '/^ZSH_THEME=/c\ZSH_THEME="powerlevel10k/powerlevel10k"' $HOME/.zshrc
+
+exec zsh
+
+# if the power10k wizard does not show up, do
+# p10k configure 
